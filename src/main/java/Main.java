5e41ef8ffecae16383ed57a7e6b5e3ff19686b9d
prostov <knockjkeee@gmail.com>
2018-10-30 17:16:30 +0300

    add last modification

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,15 @@
 import dao.UserDao;
-import modelUser.Role;
-import modelUser.User;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.sql.*;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class Main {
 
     private AtomicReference<UserDao> dao;
-    private static  AtomicReference<UserDao> bd;
+    private static AtomicReference<UserDao> bd;
     private static final String url = "jdbc:mysql://localhost:3306/monitoring?useSSL=false";
     private static final String user = "root";
     private static final String password = "root";
@@ -20,40 +21,59 @@ public class Main {
 
     public static void main(String[] args) throws ClassNotFoundException {
 
-
-
-
         try {
 
             bd = new AtomicReference<>(new UserDao());
             Class.forName("com.mysql.jdbc.Driver").newInstance();
             connection = DriverManager.getConnection(url, user, password);
             statement = connection.createStatement();
-            resultSet = statement.executeQuery("SELECT * FROM users");
-            while (resultSet.next()) {
-                int id = resultSet.getInt(1);
-                String name = resultSet.getString(2);
-                String password = resultSet.getString(3);
-                String role = resultSet.getString(4).toUpperCase();
-
-                System.out.printf("id: %d, name: %s, password: %s, role: %s", id, name, password, role);
-                System.out.println();
-
-
-
-//                System.out.println(Role.ADMIN.toString());
-//                System.out.println(Role.ADMIN);
-
-
-
-                if (Role.ADMIN.toString().equals(role)) {
-                    bd.get().add(new User(id, name, password, Role.ADMIN));
-                    System.out.println("success");
-                } else if (Role.USER.toString().equals(role)) {
-                    bd.get().add(new User(id, name, password, Role.USER));
-                    System.out.println("success");
-                }
+            File image = new File("D:\\Project\\webServlet\\src\\main\\webapp\\WEB-INF\\logo_company\\Газпром.png");
+            FileInputStream in = null;
+            try {
+                in = new FileInputStream(image);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
             }
+//            Blob blob = in.ge
+//            System.out.println(image.getAbsolutePath());
+//            String query = "INSERT INTO company ('1', 'Газпром'," + image + ", )";
+            String query = "INSERT INTO company  (id, name, description, image) VALUES (?,?,?,?)";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            preparedStatement.setInt(1, 1);
+            preparedStatement.setString(2, "Газпром");
+            preparedStatement.setString(3, "text");
+            preparedStatement.setBinaryStream(4, in);
+            preparedStatement.executeUpdate();
+
+//            preparedStatement.execute();
+
+//            resultSet = statement.executeQuery("INSERT INTO company VALUES ('1', 'Газпром', , )");
+//            while (resultSet.next()) {
+//                int id = resultSet.getInt(1);
+//                String name = resultSet.getString(2);
+//                String password = resultSet.getString(3);
+//                String role = resultSet.getString(4).toUpperCase();
+//
+//                System.out.printf("id: %d, name: %s, password: %s, role: %s", id, name, password, role);
+//                System.out.println();
+//
+//
+//
+////                System.out.println(Role.ADMIN.toString());
+////                System.out.println(Role.ADMIN);
+//
+//
+//
+//                if (Role.ADMIN.toString().equals(role)) {
+//                    bd.get().add(new User(id, name, password, Role.ADMIN));
+//                    System.out.println("success");
+//                } else if (Role.USER.toString().equals(role)) {
+//                    bd.get().add(new User(id, name, password, Role.USER));
+//                    System.out.println("success");
+//                }
+//            }
 
 
         } catch (SQLException e) {
@@ -82,9 +102,6 @@ public class Main {
 
 
     }
-
-
-
 
 
 //    private static void connectionSQL() throws SQLException {
--- src/main/java/servlets/servlet/CompanyServlet.java
+++ src/main/java/servlets/servlet/CompanyServlet.java
@@ -4,7 +4,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.io.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 //@WebServlet("/CompanyServlet")
 public class CompanyServlet extends HttpServlet {
@@ -13,6 +16,9 @@ public class CompanyServlet extends HttpServlet {
 
         String name = req.getParameter("param1");
         req.getSession().setAttribute("name", name);
+
+        addBLOBtoDB();
+
 
         System.out.println(name);
         req.getRequestDispatcher("/WEB-INF/view/company.jsp").forward(req, resp);
@@ -31,5 +37,87 @@ public class CompanyServlet extends HttpServlet {
 //        System.out.println("td8:"+td8);
 
 //        req.getRequestDispatcher("/WEB-INF/view/company.jsp").forward(req, resp);
+    }
+
+
+    private void addBLOBtoDB() {
+        Connection connection = (Connection) getServletContext().getAttribute("DBConnection");
+//        File file = new File(getServletContext().getRealPath("/"), "\\WEB-INF\\logo_company\\Газпром.png");
+//        File file = new File(getServletContext().getRealPath("/"), "\\WEB-INF\\logo_company\\Лукойл.png");
+        File file = new File(getServletContext().getRealPath("/"), "\\WEB-INF\\logo_company\\Роснефть.png");
+        FileInputStream stream;
+
+        try {
+            stream = new FileInputStream(file);
+//            String query = "INSERT INTO company  (id, name, description, image) VALUES (?,?,?,?)";
+            String query = "INSERT INTO company  (name, description, image) VALUES (?,?,?)";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+//            preparedStatement.setInt(1, 1);
+            preparedStatement.setString(1, "Роснефть");
+            preparedStatement.setString(2, "text");
+            preparedStatement.setBinaryStream(3, stream);
+            preparedStatement.executeUpdate();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        String fileName = "toto.png";
+        File fileWrite = new File(fileName);
+
+//        String pathUpload = "\\WEB-INF\\logo_company\\";
+        FileInputStream in;
+        FileOutputStream out;
+
+        try {
+            int count;
+            in = new FileInputStream(file);
+            out = new FileOutputStream(fileWrite);
+            while ((count = in.read()) != -1) {
+                out.write((char)count);
+            }
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Success");
+
+        }
+
+        if (fileWrite.exists()) {
+            try {
+                fileWrite.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println("Create!!!!");
+            }
+        }
+
+
+        System.out.println(file.getName());
+        System.out.println(fileWrite.getName());
+        System.out.println(file.length());
+        System.out.println(fileName.length());
+
+
+
+
+
+//        String query = "INSERT INTO company  (id, name, description, image) VALUES (?,?,?,?)";
+//
+//        try {
+//            PreparedStatement preparedStatement = connection.prepareStatement(query);
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
     }
 }
